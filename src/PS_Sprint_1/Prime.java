49d9b6d9de19581128d52ea6ac82fd86a044247e
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2025-01-09 22:40:57 +0530

    added for pattern

---
+++ src/PS_Sprint_1/Prime.java
@@ -0,0 +1,33 @@
+package PS_Sprint_1;
+
+import java.util.Scanner;
+
+public class Prime {
+    static boolean isPrime(int n)
+    {
+        // Corner case
+        if (n <= 1)
+            return false;
+
+        // Check from 2 to n-1
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    // Driver Program
+    public static void main(String args[])
+    {
+        if (isPrime(11))
+            System.out.println(" Prime");
+        else
+            System.out.println(" Not Prime");
+        if (isPrime(15))
+            System.out.println(" Prime");
+        else
+            System.out.println(" Not Prime");
+    }
+
+}
