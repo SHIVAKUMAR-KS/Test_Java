91f1ddd393db59bd2052228281694f27edb2f274
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2025-01-09 22:29:45 +0530

    added for pattern

---
+++ src/Arrays_GFG/Reverse_Arr.java
@@ -0,0 +1,10 @@
+package Arrays_GFG;
+
+public class Reverse_Arr {
+    public static void main(String[] args) {
+        int arr[]={4,6,3,2,};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
