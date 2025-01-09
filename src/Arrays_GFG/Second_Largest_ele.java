75bbaddc950626fad95be0cd7d8fb4496f156e2a
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2025-01-09 18:57:01 +0530

    added new code

--- src/Arrays_GFG/Second_Largest_ele.java
+++ src/Arrays_GFG/Second_Largest_ele.java
@@ -8,7 +8,7 @@ public class Second_Largest_ele {
         int n=arr.length;
         Arrays.sort(arr);//2,4,5,6,9
 
-        //as we know the  n-1 will be the second largest
+        //as we know the  n-1 will be th second largest
        // System.out.println(arr[n-2]);
         for(int i=n-2;i>=0;i--){
             if(arr[i] !=arr[n-1]){
