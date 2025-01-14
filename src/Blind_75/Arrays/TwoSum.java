acac413596d2526ecd76d664745b59cf5efad927
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2025-01-14 21:27:50 +0530

    added code for 2 pointer

---
+++ src/Blind_75/Arrays/TwoSum.java
@@ -0,0 +1,26 @@
+package Blind_75.Arrays;
+
+public class TwoSum {
+    // TC : O(n)
+    // SC: O(n)
+    public static List<Integer> twoSum(int nums[], int target) {
+        // Your code here
+        HashSet<Integer> seen = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : nums) {
+            int complement = target - num;
+            if (seen.contains(complement)) {
+                result.add(complement);
+                result.add(num);
+                return result; // Return the pair as a List
+            }
+            seen.add(num);
+        }
+
+
+        return result;
+    }
+
+
+}
