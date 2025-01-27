68e3a0f0c299b60216840a99bcdd877297d9c157
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2025-01-27 18:55:45 +0530

    added code for two pointer

---
+++ src/Blind_75/Two_Pointer/moving_zero.java
@@ -0,0 +1,21 @@
+package Blind_75.Two_Pointer;
+
+public class moving_zero {
+
+    /*
+        public void moveZeroes(int[] nums) {
+            int NonZeroPointer=0;
+
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]!=0){
+                    int temp=nums[i];
+                    nums[i]=nums[NonZeroPointer];
+                    nums[NonZeroPointer]=temp;
+                    NonZeroPointer++;
+
+                }
+            }
+
+        }
+    */
+}
