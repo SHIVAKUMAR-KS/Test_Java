7a72b0bb2825f0befcdfaf904082844dcde64980
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2025-01-29 10:12:57 +0530

    added code for right angles triangle with ulta also

--- src/Pattern/Right_Triangle/left.java
+++ src/Pattern/Right_Triangle/left.java
@@ -7,20 +7,35 @@ public class left {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
                 System.out.print("*"+" ");
             }
             System.out.println();
         }
+
+//        for(int i=n;i>=0;i--){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("*"+" ");
+//            }
+//            System.out.println();
+//        }
     }
 
 }
 /*
-4
+5
 *
 * *
 * * *
 * * * *
+* * * * *
 
+
+5
+* * * * *
+* * * *
+* * *
+* *
+*
  */
