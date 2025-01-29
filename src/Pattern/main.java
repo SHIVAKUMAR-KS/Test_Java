a10afb4de4e45ec0a79fb5ad8caae6a2b5c647f7
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2025-01-29 10:50:05 +0530

    added new updated code

--- src/Pattern/main.java
+++ src/Pattern/main.java
@@ -6,7 +6,21 @@ public class main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        /*
         for (int i = 1; i <= n; i++) {
+            // Print dashes
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");// two space or else - one space
+            }
+            // Print stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+         */
+        for (int i = n; i >=0; i--) {
             // Print dashes
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");// two space or else - one space
